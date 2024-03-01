7e18ad0a931a31572acaec14bebad5b73271b4d3
Miguel Francischini <migueldev29@gmail.com>
2024-03-01 08:04:02 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/migueldev/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.migueldev.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.migueldev.workshopmongo.domain.User;
+import com.migueldev.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
